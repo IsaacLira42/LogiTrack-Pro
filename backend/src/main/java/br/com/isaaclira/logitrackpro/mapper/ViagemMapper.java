5018a9a1e1de52651d54b91aaa09cbb39343f6fd
IsaacLira42 <isaaclira422@gmail.com>
2026-07-24 17:45:39 -0300

    refactor: configura ignorar campos id e veiculo no ViagemMapper

--- backend/src/main/java/br/com/isaaclira/logitrackpro/mapper/ViagemMapper.java
+++ backend/src/main/java/br/com/isaaclira/logitrackpro/mapper/ViagemMapper.java
@@ -4,9 +4,12 @@ import br.com.isaaclira.logitrackpro.dto.request.ViagemRequestDTO;
 import br.com.isaaclira.logitrackpro.dto.response.ViagemResponseDTO;
 import br.com.isaaclira.logitrackpro.model.Viagem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = VeiculoMapper.class)
 public interface ViagemMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "veiculo", ignore = true)
     Viagem toEntity(ViagemRequestDTO dto);
     ViagemResponseDTO toResponseDTO(Viagem viagem);
 }

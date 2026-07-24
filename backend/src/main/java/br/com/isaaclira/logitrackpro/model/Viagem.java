feb1c71d20de21ef278b616bc86ff24eb3fc0309
IsaacLira42 <isaaclira422@gmail.com>
2026-07-24 16:30:02 -0300

    fix: corrige configuraçcoo do Flyway e atualiza propriedades do banco de dados

--- backend/src/main/java/br/com/isaaclira/logitrackpro/model/Viagem.java
+++ backend/src/main/java/br/com/isaaclira/logitrackpro/model/Viagem.java
@@ -1,7 +1,6 @@
 package br.com.isaaclira.logitrackpro.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;

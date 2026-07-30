69428c7c7eb1dd6e64393d102ff24f703a417c7f
IsaacLira42 <isaaclira422@gmail.com>
2026-07-29 21:36:56 -0300

    configura cors para frontend em producao

--- backend/src/main/java/br/com/isaaclira/logitrackpro/config/CorsConfig.java
+++ backend/src/main/java/br/com/isaaclira/logitrackpro/config/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -14,11 +15,20 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://logitrack-pro-lyart.vercel.app"
+                        )
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "PATCH",
+                                "OPTIONS"
+                        )
                         .allowedHeaders("*");
             }
-
         };
     }
 }
